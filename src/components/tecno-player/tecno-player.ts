6e1d9cbfee731a336e49f0434c8e9cb68b450fcd
Jose Salazar <jsalazar3300@gmail.com>
2017-04-08 12:33:39 -0430

    10 - Componentes - Parte II

--- src/components/tecno-player/tecno-player.ts
+++ src/components/tecno-player/tecno-player.ts
@@ -11,23 +11,37 @@ export class TecnoPlayerComponent implements OnInit {
   @Input() cover:    string;
   @Input() audioSrc: string;
 
-  public audio:   any;
-  public ready:   boolean;
-  public playing: boolean;
+  public audio:         any;
+  public ready:         boolean;
+  public playing:       boolean;
+  public audioPos:      number;
+  public audioDuration:  number;
   
   constructor() {
-    this.ready   = false;
-    this.playing = false;
+    this.ready    = false;
+    this.playing  = false;
+    this.audioPos = 0;
   }
 
   ngOnInit() {
-    this.audio = new Audio();
+    this.audio     = new Audio();
     this.audio.src = this.audioSrc;
     this.audio.load();
 
     this.audio.oncanplaythrough = () => {
-      this.ready = true;
+      this.ready         = true;
+      this.audioDuration = this.audio.duration;
     };
+
+    this.audio.ontimeupdate = (event) => {
+      this.audioPos = this.audio.currentTime;
+    };
+
+    this.audio.onended = () => {
+      this.audioPos = 0;
+      this.playing  = false;
+    };
+
   }
 
   togglePlay(){
